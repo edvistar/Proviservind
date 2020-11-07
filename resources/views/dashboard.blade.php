bf48fcf6ccca01b629d2f3740c5985baf63e1993
edvistar <victorhoyoscolombia@gmail.com>
2020-11-06 19:38:55 -0500

    Editando Jetstream

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,18 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            PROVISERVIND
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <P>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi, temporibus numquam asperiores est quis ab, omnis necessitatibus aspernatur quisquam dolorem fugit voluptate sequi, nisi totam voluptatum adipisci! Nemo, unde voluptates!</P>
+                <P>Delectus excepturi numquam reprehenderit aspernatur deserunt, quo accusantium. Vitae, neque sequi? Quod veniam nesciunt, quaerat exercitationem blanditiis cumque quis, quia delectus animi sunt similique quae adipisci, aspernatur reprehenderit neque explicabo?</P>
+                <P>Autem hic atque expedita dolorem, adipisci, nisi mollitia quidem corrupti earum, nihil consequatur commodi! Facilis recusandae magni et perferendis corporis. Facere maxime deserunt consequuntur cumque porro reiciendis? Exercitationem, architecto cumque.</P>
+                <P>Sequi consequuntur accusantium asperiores vero? Debitis facere suscipit sunt atque, necessitatibus dolore repudiandae sint, unde modi, consectetur facilis sapiente dolorem. Rem ipsa vero consectetur consequatur sunt sed, deserunt quasi. Consequatur!</P>
+                <P>Id aut consequuntur laboriosam aspernatur, mollitia dolor eos! Eligendi quis unde pariatur repellendus esse tenetur rem consectetur. Error dicta, itaque architecto ipsam voluptate temporibus ad ducimus repudiandae, sapiente, cumque optio.</P>
             </div>
         </div>
     </div>
